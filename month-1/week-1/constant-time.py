663bb9a43358a5e81d868434abb0a27bfdceb0e8
romeo <romeomeshack34@gmail.com>
2025-01-15 11:33:48 +0100

    Constant Time Complexity

---
+++ month-1/week-1/constant-time.py
@@ -0,0 +1,7 @@
+'''No matter the size of the input, the operation count remain constant: 0(1)'''
+def get_first_element(arr):
+    return arr[0]
+
+arr = [10, 20, 30, 40, 50, 60, 70]
+
+print(get_first_element(arr))

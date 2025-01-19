1487e832fcba8e000b4bfb9077c5f0b6bb386e8f
romeo <romeomeshack34@gmail.com>
2025-01-19 10:01:34 +0100

    Common string operations

---
+++ month-1/week-2/common-string-operations.py
@@ -0,0 +1,14 @@
+# Concatenation
+s1, s2 = "Hello", "World!"
+print(s1 + " " + s2) # Output: "Hello world!"
+
+# Slicing
+s = "abcdef"
+print(s[1:4])  # Output: "bcd"
+
+# Searching
+print("abc" in "abcdef")  # Output: True
+
+# Replacing
+s = "banana"
+print(s.replace("na", "xy"))  # Output: "baxyxy"
